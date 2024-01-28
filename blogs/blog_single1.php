a93c769b26b115dd83a965505c94fe74c8d41ce0
Sparkles1319 <71546649+Sparkles1319@users.noreply.github.com>
2024-01-28 02:13:46 -0500

    Add files via upload

---
+++ blogs/blog_single1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ijeoma
+ * Date: 11/07/2018
+ * Time: 11:32
+ */
+?>
+
---
+++ careers/header.php
@@ -0,0 +1,55 @@
+<?php include_once "variables_file.php";
+//echo substr(__FILE__, 0, -strlen($_SERVER['SCRIPT_NAME']));
+?>
+<?php include_once("analyticstracking.php") ?>
+<head>
+    <!--===============================================
+    Template Design By WpFreeware Team.
+    Author URI : http://www.wpfreeware.com/
+    ====================================================-->
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>New Hope Hospital Onitsha: <?php echo $sub_title;?></title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="<?php echo $base_url.'/';?>images/New_Hope_Logo.jpg"/>
+
+    <!-- CSS
+    ================================================== -->
+    <!-- Bootstrap css file-->
+    <link href="<?php echo $base_url.'/';?>css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font awesome css file-->
+    <link href="<?php echo $base_url.'/';?>css/font-awesome.min.css" rel="stylesheet">
+    <!-- Default Theme css file -->
+    <link id="switcher" href="<?php echo $base_url.'/';?>css/themes/hospital-orange-theme.css?<?php echo uniqid();?>" rel="stylesheet">
+    <!-- Slick slider css file -->
+    <link href="<?php echo $base_url.'/';?>css/slick.css" rel="stylesheet">
+    <!-- Photo Swipe Image Gallery -->
+    <link rel='stylesheet prefetch' href='<?php echo $base_url.'/';?>css/photoswipe.css'>
+    <link rel='stylesheet prefetch' href='<?php echo $base_url.'/';?>css/default-skin.css'>
+
+    <!-- Main structure css file -->
+    <link href="<?php echo $base_url.'/';?>style.css?<?php echo uniqid();?>" rel="stylesheet">
+
+    <!-- Google fonts -->
+<!--    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>-->
+<!--    <link href='http://fonts.googleapis.com/css?family=Habibi' rel='stylesheet' type='text/css'>-->
+<!--    <link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative:900' rel='stylesheet' type='text/css'>-->
+
+    <!-- Hospital icons-->
+    <link rel="stylesheet" href="<?php echo $base_url.'/';?>hospital_icon/packages/webfont-medical-icons/wfmi-style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
